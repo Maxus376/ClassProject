944b02888b421b3af47a9c0a1dec5529d6da4a88
mpredein1020@gmail.com <mpredein1020@gmail.com>
2022-02-23 23:48:11 +0300

    Добавьте файлы проекта.

---
+++ ClassProject/Apteka.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassProject
+{
+    public class Apteka
+    {
+        public string name { get; set; }
+        List<Medicines> medicines;
+
+        public Apteka()
+        {
+            medicines = new List<Medicines>();
+        }
+        public void Add(Medicines add_medicines)
+        {
+            medicines.Add(add_medicines);
+        }
+
+        public void DetectExpensiveMedicines()
+        {
+            float expensive = 0f;
+            string expensive_name = "";
+            foreach (var variable in medicines)
+            {
+                if (expensive < variable.price)
+                {
+                    expensive = variable.price;
+                    expensive_name = variable.name;
+                }
+            }
+            Console.WriteLine($"Самое дорогое лекарство: {expensive_name}, его цена: {expensive} руб.");
+        }
+        public void GetSumPriceMedicines()
+        {
+            float sum = 0f;
+            foreach (var variable in medicines)
+            {
+                sum += variable.price;
+            }
+            Console.WriteLine($"Сумма стоимости всех лекарств: {sum} руб.");
+        }
+    }
+    public class Medicines
+    {
+        public string name { get; set; }
+        public float price { get; set; } 
+    }
+}
---
+++ ClassProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+namespace ClassProject
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Apteka apt1 = (new Apteka { name = "Губернская"});
+            ShowName(apt1.name);
+            apt1.Add(new Medicines { name = "Амбробене", price = 390f });
+            apt1.Add(new Medicines { name = "Гексорал", price = 500f });
+            apt1.Add(new Medicines { name = "Гевискон", price = 900f });
+            apt1.Add(new Medicines { name = "Парацетамол", price = 150f });
+            apt1.DetectExpensiveMedicines();
+            apt1.GetSumPriceMedicines();
+            ReadKey();
+        }
+
+        static void ShowName(string name)
+        {
+            WriteLine($"Название аптеки: {name}");
+        }
+    }
+}
